bd8bfd7a0b4fda865294da1b996e1c263ad01ff6
Andreas Rümpel <ruempel@gmail.com>
2021-12-07 20:45:18 +0100

    Update dependency versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("org.springframework.boot") version "2.5.6"
+    id("org.springframework.boot") version "2.6.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
     java
 }
@@ -16,7 +16,7 @@ dependencies {
     annotationProcessor("org.projectlombok:lombok")
 
     // JUnit
-    implementation(platform("org.junit:junit-bom:5.8.1"))
+    implementation(platform("org.junit:junit-bom:5.8.2"))
     testImplementation("org.junit.jupiter:junit-jupiter-api") // test API
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine") // test engine implementation
     testImplementation("org.springframework.boot:spring-boot-starter-test")
