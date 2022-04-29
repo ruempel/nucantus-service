925dac4c535f0ab869b89741558ff85a5fe4dcaa
Andreas Rümpel <ruempel@gmail.com>
2022-04-29 22:19:52 +0200

    Remove unused header

--- src/main/java/de/nucantus/rest/CORSResponseFilter.java
+++ src/main/java/de/nucantus/rest/CORSResponseFilter.java
@@ -4,6 +4,8 @@ import javax.ws.rs.container.ContainerRequestContext;
 import javax.ws.rs.container.ContainerResponseContext;
 import javax.ws.rs.container.ContainerResponseFilter;
 import javax.ws.rs.core.MultivaluedMap;
+import javax.ws.rs.core.Response;
+import java.net.http.HttpRequest;
 
 /**
  * Cross-origin resource sharing filter to be registered in resource configuration of application.
@@ -14,6 +16,6 @@ public class CORSResponseFilter implements ContainerResponseFilter {
         MultivaluedMap<String, Object> headers = responseContext.getHeaders();
         headers.add("Access-Control-Allow-Origin", "*");
         headers.add("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
-        headers.add("Access-Control-Allow-Headers", "Content-Type, api_key, Authorization");
+        headers.add("Access-Control-Allow-Headers", "Content-Type, Authorization");
     }
 }
