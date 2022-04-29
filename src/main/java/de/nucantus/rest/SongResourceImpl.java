3635de8b26bbb431c316728bcf3b72649ad355d2
Andreas Rümpel <ruempel@gmail.com>
2022-04-29 22:20:50 +0200

    Use component annotation for resource

--- src/main/java/de/nucantus/rest/SongResourceImpl.java
+++ src/main/java/de/nucantus/rest/SongResourceImpl.java
@@ -1,20 +1,14 @@
 package de.nucantus.rest;
 
 import de.nucantus.service.SongService;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 import javax.ws.rs.core.Response;
 
-/**
- * This resource implements the song browser API.
- */
+@Component
 public class SongResourceImpl implements SongResource {
     private final SongService service;
 
-    /**
-     * @param service song service
-     */
-    @Autowired
     public SongResourceImpl(SongService service) {
         this.service = service;
     }
