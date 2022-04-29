a5f038a5f96ecd805b8c7a8348fec67bd4da54cf
Andreas Rümpel <ruempel@gmail.com>
2022-04-29 22:20:33 +0200

    Use logger annotation

--- src/main/java/de/nucantus/rest/ChallengeResourceImpl.java
+++ src/main/java/de/nucantus/rest/ChallengeResourceImpl.java
@@ -4,9 +4,7 @@ import de.nucantus.model.Challenge;
 import de.nucantus.model.ChallengeCreator;
 import de.nucantus.model.ChallengeState;
 import de.nucantus.service.ChallengeService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.core.Context;
@@ -15,28 +13,20 @@ import javax.ws.rs.core.UriBuilder;
 import javax.ws.rs.core.UriInfo;
 import java.util.NoSuchElementException;
 
-/**
- * This resource implements the challenge management API.
- */
 @Component
+@Slf4j
 public class ChallengeResourceImpl implements ChallengeResource {
-    private static final Logger LOG = LoggerFactory.getLogger(ChallengeResourceImpl.class);
-
     private final ChallengeService service;
     @Context
     private UriInfo uriInfo;
 
-    /**
-     * @param service challenge service
-     */
-    @Autowired
     public ChallengeResourceImpl(ChallengeService service) {
         this.service = service;
     }
 
     @Override
     public Response challengesPost(ChallengeCreator challengeCreator) {
-        LOG.info("Challenge received for " + challengeCreator.getSongId()
+        log.info("Challenge received for " + challengeCreator.getSongId()
                 + " from player " + challengeCreator.getChallengingPlayer());
 
         Challenge challenge = service.addChallenge(challengeCreator);
@@ -49,7 +39,7 @@ public class ChallengeResourceImpl implements ChallengeResource {
 
     @Override
     public Response challengesGet(ChallengeState state) {
-        LOG.info("Request challenges with state " + state);
+        log.info("Request challenges with state " + state);
 
         return switch (state) {
             case OPEN -> Response.ok().entity(service.getOpenChallenges()).build();
@@ -60,23 +50,23 @@ public class ChallengeResourceImpl implements ChallengeResource {
 
     @Override
     public Response challengesIdPut(int id, String joiningPlayer) {
-        LOG.info("Challenge accepted for " + id + " from player " + joiningPlayer);
+        log.info("Challenge accepted for " + id + " from player " + joiningPlayer);
 
         try {
             Challenge challenge = service.joinChallenge(id, joiningPlayer);
             return Response.ok(challenge).build();
         } catch (NoSuchElementException e) {
-            return Response.status(Response.Status.NOT_FOUND.getStatusCode(), "Challenge not found").build();
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
     }
 
     @Override
     public Response challengesIdDelete(int id) {
         if (service.deleteChallenge(id)) {
-            LOG.info("Deleted song from accepted list: " + id);
+            log.info("Deleted song from accepted list: " + id);
             return Response.noContent().build();
         } else {
-            return Response.status(Response.Status.NOT_FOUND.getStatusCode(), "Challenge not found").build();
+            return Response.status(Response.Status.NOT_FOUND).build();
         }
     }
 }
